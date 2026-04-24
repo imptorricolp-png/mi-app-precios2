da99b56b93690e216be0129082a9e3558342ebaf
imptorricolp-png <imp.torrico.lp@gmail.com>
2026-04-24 16:26:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import os  # <--- ASEGÚRATE DE QUE ESTA LÍNEA ESTÉ AQUÍ
+import os  
 from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
 
