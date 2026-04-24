9e033a2388ce5eaca82b16a3c19038fbffd0cfc1
imptorricolp-png <imp.torrico.lp@gmail.com>
2026-04-24 15:57:51 -0400

    subiendo 2

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ df = pd.read_excel("precios.xlsx")
 
 # 3. Configurar la IA (Necesitas una API Key de OpenAI)
 # Nota: OpenAI da créditos gratis al inicio, pero requiere registro.
-os_api_key = "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" 
+if "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" in st.secrets:
+    os.environ["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"] = st.secrets["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"]
+else:
+    os.environ["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"] = "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"
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=os_api_key)
 
 # 4. Crear el Agente que lee el Excel
